ba9276c6a8e581b15c4dd396d0acf08f53986526
thedean111 <81821994+thedean111@users.noreply.github.com>
2025-10-30 18:34:42 -0400

    Had to uncomment part of the intro

--- src/galaxy/hud/IntroSequence.js
+++ src/galaxy/hud/IntroSequence.js
@@ -28,11 +28,10 @@ export default class IntroSequence {
         })
 
         this.textMgr.setText('#introText', [
-            '> Running arrival sequence^750.^750.^750.^750<br>>^250<br>'
-            // + 
-            // '> Activity-B4DR 1900.65.69<br>>> ' + date + '<br>>> ' + time + '^750<br>>^100<br>>^100<br>' + 
-            // '> Approaching Subsystem:^250<br>>> name-<br>>> D34N-B4DR<br>>^250<br>>> type- <br>>> P0R7F0L10<br>>^250<br>>> GCS- <br>>> <i>l</i> = 08°<br>>> <i>b</i> = 13°<br>>^250<br>' +
-            // '> Displaying VCam™ 3 video stream^750.^750.^750.^500'
+            '> Running arrival sequence^750.^750.^750.^750<br>>^250<br>' + 
+            '> Activity-B4DR 1900.65.69<br>>> ' + date + '<br>>> ' + time + '^750<br>>^100<br>>^100<br>' + 
+            '> Approaching Subsystem:^250<br>>> name-<br>>> D34N-B4DR<br>>^250<br>>> type- <br>>> P0R7F0L10<br>>^250<br>>> GCS- <br>>> <i>l</i> = 08°<br>>> <i>b</i> = 13°<br>>^250<br>' +
+            '> Displaying VCam™ 3 video stream^750.^750.^750.^500'
         ], true, true, 10, () => {
             // Play the text flicker animation when done
             this.wrapper.classList.add('text-off');
